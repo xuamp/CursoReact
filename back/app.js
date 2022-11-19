2810cd65e900605aa3821f1f236ce8a330b97282
xuamp <108774140+xuamp@users.noreply.github.com>
2022-11-19 20:05:42 -0300

    Add files via upload

--- back/app.js
+++ back/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+var cors = require('cors');
 
 require('dotenv').config();
 var session = require('express-session');
@@ -11,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -44,10 +47,16 @@ secured = async (req, res, next) => {
   }
 }
 
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 
 // catch 404 and forward to error handler
--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -2,10 +2,35 @@ var express = require('express');
 const pool = require('../../models/bd');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel')
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+var uploader = util.promisify(cloudinary.uploader.upload);
+var destroy = util.promisify(cloudinary.uploader.destroy);
+
 
 router.get('/', async function(req, res, next) {
 
 var novedades = await novedadesModel.getNovedades();
+
+
+novedades = novedades.map(novedad => {
+  if (novedad.img_id) {
+    const imagen = cloudinary.image(novedad.img_id,{
+      width: 100,
+      height: 100,
+      crop: 'fill'
+    });
+    return{
+      ...novedad,
+      imagen
+    }
+  } else {
+    return{ 
+      ...novedad,
+      imagen: ''
+    }
+  }
+})
 
   res.render('admin/novedades', {
     layout: 'admin/layout',
@@ -22,8 +47,19 @@ router.get('/agregar', function (req, res, next) {
 
 router.post('/agregar', async (req, res, next) => {
   try{
+    var img_id = '';
+    console.log(req.files.imagen);
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
     if (req.body.Titulo != "" && req.body.Copete != "" && req.body.Cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -44,6 +80,12 @@ router.post('/agregar', async (req, res, next) => {
 
 router.get('/eliminar/:id', async (req, res, next) => {
    var id = req.params.id;
+
+   let novedad = await novedadesModel.getNovedadesById(id);
+   if(novedad.img_id) {
+    await (destroy(novedad.img_id));
+   }
+
    await novedadesModel.deleteNovedadesById(id);
    res.redirect('/admin/novedades');
 })
@@ -59,10 +101,34 @@ router.get('/modificar/:id', async (req, res, next) => {
 router.post('/modificar', async (req, res, next) => {
   try {
 
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+
+    if(req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+      if(req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id
+        borrar_img_vieja = true;
+      }
+      } else {
+        if(req.files && Object.keys(req.files).length > 0) {
+          imagen = req.files.imagen;
+          img_id = (await uploader(imagen.tempFilePath)).public_id
+          borrar_img_vieja = true;
+        }
+      }
+
+      if(borrar_img_vieja && req.body.img_original) {
+        await (destroy(req.body.img_original));
+      }
+
     var obj = {
       Titulo: req.body.Titulo,
       Copete: req.body.Copete,
-      Cuerpo: req.body.Cuerpo
+      Cuerpo: req.body.Cuerpo,
+      img_id
     }
 
     await novedadesModel.modificarNovedadById(obj, req.body.id);
